9eb0420534a16b6ddc623b5849b33039cd1e5aa8
Nathan Lamont <nathan@bigggerplanet.com>
2023-07-14 11:45:00 -0400

    README, scss organization, a few comments

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
+# Q3 Paginated Table Exercise
+
+Live version at: https://main--cool-eclair-be6f40.netlify.app
+
+Requires Node 16 to build.
+
+Vue front end using (somewhat unnecessarily) Nuxt. I used [Vuetify](https://vuetifyjs.com), which I hadn't actually used before, because it had select and pagination controls which looked close to the original design.
+
+## Notes:
+
+- I changed the format of the returned value from the supplied `getRecommendation` function to include the total number of records, since the api would be able to efficiently supply this number and the example was "fetching" all records anyway. In a real server, that information might instead be included in a response header to keep the result data "pure."
+- The sample video seemed to show that the procedure columns also changed between clinics (the first had no SCS column) but there was no data for that in `getNameMappings()` so I elected to just show the SCS column regardless.
+- For the purposes of this exercise, I just included some scss to make the Vuetify components look more like the design. There appear to be more proper was to customize it (like setting scss variables) but I did not look deeply into it.
+
+Below is the default Nuxt README:
+
 # Nuxt 3 Minimal Starter
 
 Look at the [Nuxt 3 documentation](https://nuxt.com/docs/getting-started/introduction) to learn more.
--- pages/index.vue
+++ pages/index.vue
@@ -2,66 +2,74 @@
 import { getRecommendation, getNameMappings } from '../api';
 
 const allProcedureTypes = ['SCS', 'BVN', 'SIJ'] as const;
-type procedureType = (typeof allProcedureTypes)[number];
+type ProcedureType = (typeof allProcedureTypes)[number];
 
-type recommendationType = {
+type RecommendationType = {
   clinicId: string;
-  patientAttributes: Record<string, string>;
+  patientAttributes: Record<string, string | undefined>;
   recDate: string;
-  recommendedProcedures: procedureType[];
+  recommendedProcedures: { name: ProcedureType }[];
 };
 
-type nameMappingType = {
+type RecommendationResponse = {
+  totalPages: number;
+  totalRecords: number;
+  data: RecommendationType[];
+};
+
+type NameMappingType = {
   header: Record<string, string[]>;
   column: Record<string, string>;
 };
 
-type labeledSelectValue = { title: string; value: string | number };
+type LabeledSelectStringValue = { title: string; value: string };
 
-const recommendations = ref<recommendationType[]>([]);
-const headers = ref<nameMappingType | undefined>(undefined);
+const recommendations = ref<RecommendationType[]>([]);
+const headers = ref<NameMappingType | undefined>(undefined);
 const page = ref(1);
 const pageSize = ref(3);
 const totalPages = ref(0);
 const totalRecords = ref(0);
-const clinicValues: labeledSelectValue[] = [
-  { title: 'Seattle Grace', value: 'seattle-grace' },
+const clinicValues: LabeledSelectStringValue[] = [
   { title: 'Sacred Heart', value: 'sacred-heart' },
+  { title: 'Seattle Grace', value: 'seattle-grace' },
 ];
 
-const clinicId = ref<labeledSelectValue>(clinicValues[0].value);
+const clinicId = ref<string>(clinicValues[0].value as string);
 
-onMounted(() => {
-  // getRecommendation(clinicId.value, 3).then((d) => {
-  //   recommendations.value = d.data;
-  //   totalPages.value = d.totalPages;
-  //   totalRecords.value = d.totalRecords;
-  // });
-  // refreshData();
-});
-
+// if the clinic ID changes, set the current page to 1
 watch(clinicId, () => {
   page.value = 1;
   headers.value = undefined;
   recommendations.value = [];
 });
+
+// if the page size changes, set the current page so that the original first
+// row is still visible
 watch(pageSize, (newSize, oldSize) => {
   const firstItem = oldSize * (page.value - 1);
   page.value = Math.floor(firstItem / newSize) + 1;
 });
 
+// if the clinic, page, or page size changes, fetch the data
+// (note: there are no cascading fetches when the clinic ID and page # change
+// in the same pass 👍)
 watchEffect(() => {
   getRecommendation(clinicId.value, pageSize.value, page.value).then((d) => {
-    recommendations.value = d.data;
+    recommendations.value = d.data as RecommendationType[];
     totalPages.value = d.totalPages;
     totalRecords.value = d.totalRecords;
   });
 });
 
+// if the clinic ID changes, fetch the clinic's headers/name mappings
 watchEffect(() => {
-  getNameMappings(clinicId.value).then((d) => (headers.value = d));
+  getNameMappings(clinicId.value).then(
+    (d) => (headers.value = d as NameMappingType)
+  );
 });
 
+// 1-based index of the first and last item on the current page
 const showingFirstItem = computed(() => (page.value - 1) * pageSize.value + 1);
 const showingLastItem = computed(() =>
   Math.min(showingFirstItem.value + pageSize.value - 1, totalRecords.value)
@@ -69,14 +77,14 @@ const showingLastItem = computed(() =>
 </script>
 <template>
   <div class="panel">
-    <div style="display: flex; padding: 1rem; gap: 1rem">
+    <div class="table-controls">
       <v-select
         v-model="pageSize"
         :items="[1, 3]"
         variant="outlined"
         menu-icon="mdi-chevron-down"
         density="compact"
-        style="flex-shrink: 1; flex-grow: 0"
+        class="select-page-size"
         hide-details
       />
       <v-select
@@ -89,7 +97,7 @@ const showingLastItem = computed(() =>
         hide-details
       ></v-select>
     </div>
-    <v-table v-if="headers" style="">
+    <v-table v-if="headers">
       <thead>
         <tr>
           <th
@@ -131,21 +139,17 @@ const showingLastItem = computed(() =>
             {{ r.patientAttributes[c] }}
           </td>
           <td>{{ r.recDate }}</td>
-          <td v-for="p in allProcedureTypes" :key="'rp-' + i + p">
+          <td v-for="p in allProcedureTypes" :key="'rp-' + i + p" class="check">
             <template v-if="r.recommendedProcedures.find((n) => n.name === p)">
-              <v-icon
-                size="small"
-                icon="mdi-check"
-                style="color: #c52c9c"
-              ></v-icon>
+              <v-icon size="small" icon="mdi-check"></v-icon>
             </template>
           </td>
         </tr>
       </tbody>
     </v-table>
-    <div style="display: flex; padding: 0.5rem 1rem; align-items: center">
-      <div style="flex: 0 1 50%">
-        <small style="color: #bcbbc1"
+    <div class="table-footer">
+      <div class="details">
+        <small
           >Showing {{ showingFirstItem }}
           <template v-if="showingFirstItem !== showingLastItem">
             to {{ showingLastItem }}
@@ -153,7 +157,7 @@ const showingLastItem = computed(() =>
           of {{ totalRecords }} entries
         </small>
       </div>
-      <div style="flex: 1 1 50%; align-self: flex-end">
+      <div class="pagination">
         <v-pagination
           v-model="page"
           :length="totalPages"
@@ -167,64 +171,117 @@ const showingLastItem = computed(() =>
 </template>
 
 <style lang="scss">
-// light bg color #F1C8E5
-// dark fg color #C52C9C
+$brand-fg: #c52c9c;
+$light-gray: #bcbbc1;
+$disabled-pagination-button-opacity: 0.07;
+
 html,
 body {
   background-color: #f5f4f7;
 }
+
+.panel {
+  margin: 2rem auto;
+  border-radius: 6px;
+  max-width: min(calc(100vw - 2rem), 960px);
+  box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.2);
+  background-color: white;
+}
+
+.table-controls {
+  display: flex;
+  padding: 1rem;
+  gap: 1rem;
+  .select-page-size {
+    flex: 0 1 auto;
+  }
+}
+
+table td.check {
+  color: $brand-fg;
+}
+
+.table-footer {
+  display: flex;
+  padding: 0.5rem 1rem;
+  align-items: center;
+  .details {
+    flex: 0 1 50%;
+    color: $light-gray;
+  }
+  .pagination {
+    flex: 1 1 40%;
+    align-self: flex-end;
+  }
+}
+
+///////////////////////////
+// Vuetify customization //
+///////////////////////////
+
+// outline color on focused controls
+.v-field.v-field--focused .v-field__outline {
+  color: $brand-fg;
+}
+
+// select item - brand color highlight, inset rounded corners
+.v-list-item--density-default.v-list-item--one-line {
+  min-height: 30px;
+  margin-bottom: 0.25rem;
+}
+
+.v-list-item--variant-text .v-list-item__overlay {
+  max-width: calc(100% - 1rem);
+  left: 0.5rem;
+  color: $brand-fg;
+  border-radius: 4px;
+}
+
+// table - shorter first row of table header
 .v-table .v-table__wrapper > table > thead > tr:first-child > th {
   border-bottom: none;
   padding-top: 1em;
   --v-table-header-height: 0.5em;
 }
 
-.panel {
-  margin: 2rem auto;
-  border-radius: 6px;
-  max-width: 80vw;
-  box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.2);
-  background-color: white;
+// table - top and bottom border; no border radius
+.v-table__wrapper {
+  border-top: thin solid rgba(var(--v-border-color), var(--v-border-opacity));
+  border-bottom: thin solid rgba(var(--v-border-color), var(--v-border-opacity));
+  border-radius: 0;
 }
 
-.v-pagination__list {
-  justify-content: flex-end;
-}
-.v-field.v-field--focused .v-field__outline {
-  color: #c52c9c;
-}
-
-.v-pagination__item .v-btn__overlay {
-  opacity: var(--v-border-opacity);
-}
-
-.v-pagination__item--is-active .v-btn__overlay {
-  color: #c52c9c;
-}
-.panel .v-pagination {
+// pagination - colors, backgrounds, alignment
+.v-pagination {
   --v-border-opacity: 0.05;
   .v-pagination__prev,
   .v-pagination__next {
     .v-btn__overlay {
       opacity: var(--v-border-opacity);
     }
-  }
-}
-
-.v-pagination {
-  .v-pagination__prev,
-  .v-pagination__next {
     .v-btn.v-btn--disabled {
       .v-btn__overlay {
-        opacity: 0;
+        opacity: $disabled-pagination-button-opacity;
       }
     }
   }
+  .v-btn--disabled.v-btn--variant-flat .v-btn__overlay {
+    opacity: $disabled-pagination-button-opacity;
+  }
 }
 
-.v-table__wrapper {
-  border-top: thin solid rgba(var(--v-border-color), var(--v-border-opacity));
-  border-bottom: thin solid rgba(var(--v-border-color), var(--v-border-opacity));
-  border-radius: 0;
+.v-pagination__list {
+  justify-content: flex-end;
+}
+
+.v-pagination__item .v-btn__overlay {
+  opacity: var(--v-border-opacity);
+}
+
+.v-pagination__item--is-active {
+  .v-btn__overlay,
+  .v-btn__content {
+    color: $brand-fg;
+  }
 }
 </style>
